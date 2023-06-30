68d9ec6c16b5e62b5662df03d84400434b32e327
Leo <brightspread.jo@gmail.com>
2023-06-30 14:50:28 +0900

    Code coverage test

---
+++ TDD/BankApp/BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by Leo on 2023/06/30.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
