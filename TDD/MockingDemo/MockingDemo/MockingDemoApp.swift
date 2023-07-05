ff71c3805bfa0dfa127dd0c0fad54b5c27386949
Leo <brightspread.jo@gmail.com>
2023-07-05 09:52:48 +0900

    모킹

---
+++ TDD/MockingDemo/MockingDemo/MockingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockingDemoApp.swift
+//  MockingDemo
+//
+//  Created by Leo on 2023/06/30.
+//
+
+import SwiftUI
+
+@main
+struct MockingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TDD/MockingDemo/MockingDemo/Models/LoginResponse.swift
@@ -0,0 +1,12 @@
+//
+//  LoginResponse.swift
+//  MockingDemo
+//
+//  Created by Leo on 2023/06/30.
+//
+
+import Foundation
+
+struct LoginResponse: Codable {
+    let success: Bool 
+}
