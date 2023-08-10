743791e2cf5c33740bc5a9d2cf300e1c1f94ac2a
Leo <59555700+brightspread@users.noreply.github.com>
2023-08-10 09:39:33 +0900

    [SwiftUI] List Foreach

--- Allen복습스터디/Part23/QuizGround.playground/Contents.swift
+++ Allen복습스터디/Part23/QuizGround.playground/Contents.swift
@@ -91,3 +91,63 @@ class 학생: 사람 {
 
 let 학생1 = 학생(name: "레오", 학점: "A")
 학생1.printDescription()
+
+/*
+ Jimmy
+ 1. 아래 클래스의 프로퍼티를 한 줄로 요약해보세요
+ class SomeClass1 {
+     private var _name = "이름"
+     
+     var name: String {
+         return _name
+     }
+ }
+ 
+ 2. Substring의 COW(Copy On Write)
+ var greeting = "Hello World"
+
+ let word = greeting.prefix(5) //1.이 때 word는 메모리를 차지하고 있을까요?
+
+ greeting = "Jimmy Hello" //2. 이 때 word의 메모리는 어떤 상태일까요?
+ 
+ 3.어떤 에러가 발생하고 왜 에러가 발생하는지 메모리 관점에서 설명해보세요
+ extension SomeCalculateClass {
+         let getName: String {
+                 return "이름"
+         }
+ }
+ 
+ 
+ 
+ */
+
+/*
+ Elly
+ 1. 아래 코드가 정상적으로 동작하도록 코드를 수정해보세요.
+ class ViewModel {
+     private var id: Int = 5
+ }
+
+ class ViewController {
+     
+     let viewModel: ViewModel = ViewModel()
+
+     func viewDidLoad() {
+         super.viewDidLoad()
+         
+         // 아래 코드가 정상적으로 동작하도록 코드를 수정해보세요. (모든 코드 변경 가능)
+         print(viewModel.id) // 5
+     }
+ }
+ 
+ 
+ 2. “010-1234-5678” 전화번호의 - 를 제거하는 코드와,
+    01012345678 전화번호에 4번째, 8번째 자리에 -를 추가하는 코드를 만들어보세요
+ 
+ let phoneNumber: String = "010-1234-5678"
+
+ // 010-1234-5678 -> 01012345678
+
+ // 01012345678 -> 010-1234-5678
+ ----------------------------
+ */
--- SwiftUI/PerfectGuide/Lecture9_List_ForEach/Lecture9_List_ForEach/ContentView.swift
+++ SwiftUI/PerfectGuide/Lecture9_List_ForEach/Lecture9_List_ForEach/ContentView.swift
@@ -37,8 +37,6 @@ struct ContentView: View {
             // id: 어떤 값을 id로 할 것인지,
             // 고유한 값이 있으면 그걸 쓰면 되는데,
             // 없으니까 \.self (하나하나 아이템을 고유한 것으로 사용) -> Hasable 프로토콜 준수
-
-            
             
             ForEach(locations) { location in
                 HStack {
@@ -49,7 +47,22 @@ struct ContentView: View {
             
             // Referencing initializer 'init(_:content:)' on 'ForEach' requires that 'LocationInfo' conform to 'Identifiable'
             // id 없이 쓰려면 Identifiable 이어야함
-
+            
+            
+            // index 접근
+            ForEach(0..<locations.count, id: \.self) { index in
+                // ForEach(0..<locations.count)
+                // Non-constant range: argument must be an integer literal
+                // 배열이 변경되면 out of index 발생
+                HStack {
+                    Text("\(locations[index].city)")
+                    Text("\(locations[index].weather)")
+                }
+            }.onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5){
+                    self.locations.removeAll()
+                }
+            }
         }
     }
 }
