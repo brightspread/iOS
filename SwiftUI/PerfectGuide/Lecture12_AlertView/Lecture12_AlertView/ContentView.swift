5c79a8dfc2a423a2c6796ed11483fc4396fe4818
Leo <59555700+brightspread@users.noreply.github.com>
2023-08-11 19:58:11 +0900

    [SwiftUI] Alert View

---
+++ SwiftUI/PerfectGuide/Lecture12_AlertView/Lecture12_AlertView/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Lecture12_AlertView
+//
+//  Created by Leo on 2023/08/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var isShowAlert = false
+    @State private var selectText = "x"
+
+    var body: some View {
+        VStack {
+            Spacer()
+            Button("show alert") {
+                isShowAlert.toggle()
+            }
+            .alert(isPresented: $isShowAlert) {
+                let primaryButton = Alert.Button.default(Text("done")) {
+                    selectText = "done"
+                }
+                let secondaryButton = Alert.Button.default(Text("cancel")) {
+                    selectText = "cancel"
+                }
+                
+                return Alert(title: Text("show alert"),
+                             primaryButton: primaryButton,
+                             secondaryButton: secondaryButton)
+            }
+            
+            Spacer()
+            Text("\(isShowAlert.description)")
+            Spacer()
+            Text("\(selectText.description)")
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/PerfectGuide/Lecture12_AlertView/Lecture12_AlertView/Lecture12_AlertViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lecture12_AlertViewApp.swift
+//  Lecture12_AlertView
+//
+//  Created by Leo on 2023/08/11.
+//
+
+import SwiftUI
+
+@main
+struct Lecture12_AlertViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
